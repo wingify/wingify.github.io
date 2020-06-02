d67ffe3f53d70098850e029e0d2ef66ad4f0cd25
Gaurav Nanda <gaurav.nanda@wingify.com>
2020-06-02 20:26:28 +0530

    Updates

---
+++ component---src-pages-404-js-0c6355b7aa7c186b82f7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("TJpk"),o=n.n(u),s=n("83Zx"),i=n("IpnI"),r=n.n(i);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("main",null,l.a.createElement(o.a,{title:"404: Not Found | "+r.a.siteTitle}),l.a.createElement("h1",null,"404 Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0c6355b7aa7c186b82f7.js.map
---
+++ component---src-pages-contact-js-8f1d647515267d9174b5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"Cuy+":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),c=t("TJpk"),u=t.n(c),i=t("83Zx"),o=t("IpnI"),r=t.n(o);n.default=function(){return l.a.createElement(i.a,null,l.a.createElement("main",null,l.a.createElement(u.a,{title:"Contact | "+r.a.siteTitle}),l.a.createElement("h1",null,"Contact us"),l.a.createElement("p",null,"Contact informations")))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-8f1d647515267d9174b5.js.map
