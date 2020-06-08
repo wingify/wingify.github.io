296e94d5cf6beb714075a6c342dffcdfc214b19c
Nitish Mittal <nitish.mittal@wingify.com>
2020-06-08 19:58:57 +0530

    updated sequence of labs

--- src/pages/labs.js
+++ src/pages/labs.js
@@ -15,6 +15,14 @@ const LabsPage = () => (
           __html: `
             <div class="projects">
               <div class="project-box">
+                <a href="http://github.com/wingify/lua-resty-pubsub">
+                  <img src="/images/generic_code@2x.png" width="160" height="160">
+                  <h3>lua-resty-pubsub</h3>
+                </a>
+                <p>Pubsub client driver for the <i>ngx_lua</i> using <i>cosocket</i> API.</p>
+                <span class="pill">lua</span>
+              </div>
+              <div class="project-box">
                 <a href="/across-tabs/">
                   <img src="/images/across-tabs@2x.png" width="160" height="160">
                   <h3>AcrossTabs</h3>
@@ -22,6 +30,7 @@ const LabsPage = () => (
                 <p>Easily communicate among browser tabs.</p>
                 <span class="pill">javascript</span>
               </div>
+              <div style="clear: both"></div>
               <div class="project-box">
                 <a href="/angular-time-picker/">
                   <img src="/images/time-picker@2x.png" width="160" height="160">
@@ -47,14 +56,6 @@ const LabsPage = () => (
                 <span class="pill">javascript</span>
               </div>
               <div class="project-box">
-                <a href="/dom-comparator/">
-                  <img src="/images/dom@2x.png" width="160" height="160">
-                  <h3>DOMComparator</h3>
-                </a>
-                <p>A library to analyze and compare two DOM trees.</p>
-                <span class="pill">javascript</span>
-              </div>
-              <div class="project-box">
                 <a href="http://github.com/wingify/agentredrabbit">
                   <img src="/images/generic_code@2x.png" width="160" height="160">
                   <h3>AgentRedRabbit</h3>
@@ -63,12 +64,12 @@ const LabsPage = () => (
                 <span class="pill">python</span>
               </div>
               <div class="project-box">
-                <a href="http://github.com/wingify/lua-resty-pubsub">
-                  <img src="/images/generic_code@2x.png" width="160" height="160">
-                  <h3>lua-resty-pubsub</h3>
+                <a href="/dom-comparator/">
+                  <img src="/images/dom@2x.png" width="160" height="160">
+                  <h3>DOMComparator</h3>
                 </a>
-                <p>Pubsub client driver for the <i>ngx_lua</i> using <i>cosocket</i> API.</p>
-                <span class="pill">lua</span>
+                <p>A library to analyze and compare two DOM trees.</p>
+                <span class="pill">javascript</span>
               </div>
             </div>
           ` }} />
